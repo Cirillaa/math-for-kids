9227aee786e06ff49ecff9b5c7782aaec28c62bc
Cirillaa <polinaas2010@gmail.com>
2023-10-06 23:32:36 +0300

    Addedregistration function for sending user data to a server.Added part of validation on forms

--- registration.js
+++ registration.js
@@ -7,22 +7,68 @@ let modalCloseButtons =  document.querySelectorAll('.close-button')
 
 loginButton.addEventListener('click', function() {
     loginForm.classList.remove('hide');
+    closeModal()
 })
 registrationButton.addEventListener('click', function(){
     registrationForm.classList.remove('hide');
+    closeModal()
 })
 
-for (let i=0; i <=modalCloseButtons.length; i++) {
-    modalCloseButtons[i].addEventListener('click', function() {
-        loginForm.classList.add('hide');
-        registrationForm.classList.add('hide');
-    })
+function closeModal() {
+    for (let i = 0; i <= modalCloseButtons.length; i++) {
+      modalCloseButtons[i].addEventListener("click", function () {
+        loginForm.classList.add("hide");
+        registrationForm.classList.add("hide");
+      });
+    }
 }
 
-registrationForm.addEventListener('submit', formSend);
+const isEmpty = (value) => value.trim() === "";
 
-    const formSend = async function(event) {
+    const formSend = (event) => {
         event.preventDefault();
+
+        const enteredName = document.getElementById('name').value;
+        const enteredNick = document.getElementById('nickname').value;
+        const enteredPassword = document.getElementById('password').value;
+
+        const nameValid = !isEmpty(enteredName);
+        const nickValid = !isEmpty(enteredNick);
+        const passwordValid = enteredPassword.length > 6;
+
+        const formIsValid = nameValid && nickValid && passwordValid
+
+        const userData = {
+            name: enteredName,
+            nickname: enteredNick,
+            password: enteredPassword
+        }
+
+        console.log(userData)
+
+        const sendData = async() => {
+            
+            await fetch(
+              "https://math-users-ae263-default-rtdb.firebaseio.com/users.json",
+              {
+                method: "POST",
+                body: JSON.stringify(userData),
+              }
+            );
+        }
+
+        if(!formIsValid) {
+            if(!nameValid || !nickValid || !passwordValid) {
+                let inputs = registrationForm.querySelectorAll('input');
+                for (let i=0; i <inputs.length; i++) {
+                    inputs[i].classList.add('invalid')
+                }
+            }
+            return
+        }
+
+        sendData()
     } 
 
+    registrationForm.addEventListener("submit", formSend);
 
